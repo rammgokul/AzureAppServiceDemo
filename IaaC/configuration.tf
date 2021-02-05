ec49d14a0f2bd470d412c140318fc3aed2365847
rammgokul <69805641+rammgokul@users.noreply.github.com>
2021-02-05 18:39:58 +0530

    Update configuration.tf

--- IaaC/configuration.tf
+++ IaaC/configuration.tf
@@ -12,5 +12,9 @@ terraform {
 }
 
 provider "azurerm" {
+  subscription_id = "a78de714-8edc-42c9-b29e-88badae6370c"
+  client_id = "e86cbf2a-379a-4593-b2a5-16d4be4e6cd6"
+  client_secret = "om0JzlF8kLiWs~bi1n-c-anjd_F3ZaSVD_"
+  tenant_id ="c011cb07-2434-4e9b-bff1-e1b4b78be979"
   features {}
 }
